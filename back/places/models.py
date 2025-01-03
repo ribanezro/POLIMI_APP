635804184fbb64fee3c605418edb87f90c7a0bf5
ribanezro <rafa@tibidabo.org>
2025-01-03 12:02:52 +0100

    changes migra

--- back/places/models.py
+++ back/places/models.py
@@ -11,7 +11,7 @@ class Place(models.Model):
     address = models.CharField(max_length=255, null=True, blank=True)
     rating = models.FloatField(null=True, blank=True)
     price = models.FloatField(null=True, blank=True)
-    website = models.URLField(null=True, blank=True)
+    website = models.URLField(max_length=500, null=True, blank=True)
 
 
     def __str__(self):
