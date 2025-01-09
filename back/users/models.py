fc7ebaf5a8d932cc19210f434372e200279f7b77
ribanezro <rafa@tibidabo.org>
2025-01-09 19:45:51 +0100

    Update models.py

--- back/users/models.py
+++ back/users/models.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
-class CustomUserManager(BaseUserManager):
+class CustomUserManager(UserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set.")
