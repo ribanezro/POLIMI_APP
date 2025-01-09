185c714fb6a82d6334e23010ee49f86058952c57
ribanezro <rafa@tibidabo.org>
2025-01-09 18:08:55 +0100

    solving error

--- back/users/models.py
+++ back/users/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=30, unique=True)
-    password = models.CharField(max_length=30, null=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=150, null=True)
     email = models.EmailField(unique=True)
     bio = models.TextField(blank=True, null=True)  
     first_name = models.CharField(max_length=30, blank=True, null=True)
