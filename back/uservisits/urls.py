4440b2d36f299aca3d463ded49cce7b4d56e3b06
ribanezro <rafa@tibidabo.org>
2025-01-10 13:39:09 +0100

    Update urls.py

--- back/uservisits/urls.py
+++ back/uservisits/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_visit, name='add_visit'),
-    path('/delete/<int:id>/', views.delete_visit, name='delete_visit'),
+    path('delete/<int:id>/', views.delete_visit, name='delete_visit'),
     path('update/<int:id>/', views.update_visit, name='update_visit'),
     path('<int:id>/', views.visit_detail, name='get_visit'),
     path('user/<int:user_id>/', views.user_visits, name='user_visits'),
