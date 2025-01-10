6565b0028e10cc1357903037d6539e41e9a1c7ad
ribanezro <rafa@tibidabo.org>
2025-01-10 20:00:00 +0100

    Update serializers.py

--- back/uservisits/serializers.py
+++ back/uservisits/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import UserVisit
+from places.serializers import PlaceSerializer
 
 class UserVisitSerializer(serializers.ModelSerializer):
+    place = PlaceSerializer()
     class Meta:
         model = UserVisit
         fields = '__all__'
